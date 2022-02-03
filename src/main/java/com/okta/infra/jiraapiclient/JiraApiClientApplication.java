ec70889b17937904258ece0a8ad94ce09f68139f
Pankaj Nafria <68015746+pankajnafria-okta@users.noreply.github.com>
2022-02-02 19:19:53 -0800

    First Commit - Base of JIRA Rest Api Client

---
+++ src/main/java/com/okta/infra/jiraapiclient/JiraApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.okta.infra.jiraapiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraApiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraApiClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okta/infra/jiraapiclient/JiraApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.infra.jiraapiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraApiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
